f23246fe4d20449a1986da51d0a06ff437cd8d96
Alexandru Coman <acoman@cloudbasesolutions.com>
2016-02-02 15:27:52 +0200

    Adaugă soluția pentru problema ICAO
    Soluția este: mesaj imporant python este cel mai frumos limbaj

---
+++ solutii/alex_coman/icao/from_icao.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# *-* coding: UTF-8 *-*
+
+"""
+Organizaţia Internaţională a Aviaţiei Civile propune un alfabet în care
+fiecărei litere îi este asignat un cuvânt pentru a evita problemele în
+înțelegerea mesajelor critice.
+
+Pentru a se păstra un istoric al conversațiilor s-a decis transcrierea lor
+conform următoarelor reguli:
+    - fiecare cuvânt este scris pe o singură linie
+    - literele din alfabet sunt separate de o virgulă
+
+Următoarea sarcină ți-a fost asignată:
+    Scrie un program care să primească un fișier ce conține mesajul
+    brut (scris folosind alfabetul ICAO) și generează un fișier
+    numit icao_intrare ce va conține mesajul inițial.
+
+Mai jos găsiți un dicționar ce conține o versiune a alfabetului ICAO:
+"""
+
+from __future__ import print_function
+
+ICAO = {
+    'a': 'alfa', 'b': 'bravo', 'c': 'charlie', 'd': 'delta', 'e': 'echo',
+    'f': 'foxtrot', 'g': 'golf', 'h': 'hotel', 'i': 'india', 'j': 'juliett',
+    'k': 'kilo', 'l': 'lima', 'm': 'mike', 'n': 'november', 'o': 'oscar',
+    'p': 'papa', 'q': 'quebec', 'r': 'romeo', 's': 'sierra', 't': 'tango',
+    'u': 'uniform', 'v': 'victor', 'w': 'whiskey', 'x': 'x-ray', 'y': 'yankee',
+    'z': 'zulu'
+}
+
+
+def din_icao(fisier_intrare):
+    """Funcția va primi calea către fișierul ce conține mesajul brut și
+    va genera un fișier numit icao_intrare ce va conține mesajul inițial.
+    """
+    continut = []
+    mesaj = []
+    with open(fisier_intrare) as fisier:
+        continut = fisier.readlines()
+
+    for linie in continut:
+        for cuvant in linie.split():
+            if cuvant != ICAO[cuvant[0]]:
+                print("[x] A fost detectat un cuvant invalid: %s" % cuvant)
+                return False
+            else:
+                mesaj.append(cuvant[0])
+        mesaj.append(" ")
+
+    with open("icao_intrare", "w") as fisier_iesire:
+        fisier_iesire.write("".join(mesaj))
+
+if __name__ == "__main__":
+    din_icao("mesaj.icao")
