4f19275153020d50732a0b8f794e93fb0c5b7e8d
andaElena <ungureanu.anda.elena@gmail.com>
2016-02-04 17:15:41 +0200

    rand nou la unic si cele 3 probleme de la lab

--- solutii/anda_u/unic.py
+++ solutii/anda_u/unic.py
@@ -1,4 +1,4 @@
-def gaseste(istoric):
+﻿def gaseste(istoric):
     result = 0
     for i in istoric:
         result ^= i;
@@ -6,3 +6,4 @@ def gaseste(istoric):
 if __name__ == "__main__":
     assert gaseste([1, 2, 3, 2, 1]) == 3
     assert gaseste([1, 1, 1, 2, 2]) == 1
+
