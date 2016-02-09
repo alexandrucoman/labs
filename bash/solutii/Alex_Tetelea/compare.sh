55481e5088ec0bc85574b3b1b0df019d1f1dc949
Alexandru Tetelea <lordusmd@gmail.com>
2016-02-09 14:07:11 +0200

    Adaugat fisierul compare.sh

---
+++ bash/solutii/Alex_Tetelea/compare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+function rec {
+    if [["$(diff -r "$1" "$2" | grep -c "Only")" -eq 0]]; then
+        echo "Egale"
+    else
+        echo "Diferite"
+    fi
+}
+if test "$#" -eq 2; then
+    rec "$1" "$2"
+else
+    echo "tb 2 parametri!"
+fi
+
