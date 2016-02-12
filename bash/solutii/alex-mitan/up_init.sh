204ad19195aa9a775780746221ead56e345bd964
Alex Mitan <alex.mitan@yahoo.com>
2016-02-12 16:07:50 +0200

    Added up_init.sh; enables the use of up [nr]

---
+++ bash/solutii/alex-mitan/up_init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+echo '' >> ~/.bashrc
+echo 'function up {' >> ~/.bashrc
+echo '    LVLS=$1' >> ~/.bashrc
+echo '    for i in `seq 1 $LVLS`; do' >> ~/.bashrc
+echo '        cd ../' >> ~/.bashrc
+echo '    done' >> ~/.bashrc
+echo '} ' >> ~/.bashrc
+echo '' >> ~/.bashrc
