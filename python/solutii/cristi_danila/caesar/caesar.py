2e27cd105f72c8c25d618809ca475890f183faa7
Cristi D <cristi.dnla@gmail.com>
2016-02-23 21:01:19 +0200

    Added solutin to caesar

--- python/solutii/cristi_danila/caesar/caesar.py
+++ python/solutii/cristi_danila/caesar/caesar.py
@@ -16,13 +16,32 @@ la un n pași de ea în alfabet (unde este n este un număr întreg cunoscut
 
 from __future__ import print_function
 
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def decrypt_by_cipher(message, cipher):
+    decrypted = ""
+    for charachter in message:
+        if charachter in letters:
+            index = (ord(charachter) - ord('A') + cipher) % len(letters)
+            decrypted += letters[index - 1]
+        else:
+            decrypted += charachter
+    return decrypted
 
 def decripteaza_mesajul(mesaj):
-    """Funcția va primi un mesaj criptat folosind cifrul lui Caesar și
-    va încearca să îl decripteze.
-    """
-    pass
-
+    working_copy = mesaj.upper();
+    cipher = -1
+    
+    for n in range(1, len(letters) + 1):
+        sample = decrypt_by_cipher(working_copy, n)[:3:] 
+        if sample == "AVE":
+            cipher = n
+            break
+    
+    if cipher != -1:
+        return decrypt_by_cipher(working_copy, cipher).lower()
+    else:
+        raise Exception("Can't decrypt the message")
 
 def main():
     """ Main function docstring """
@@ -35,7 +54,7 @@ def main():
         return
 
     for mesaj in mesaje.splitlines():
-        decripteaza_mesajul(mesaj)
+        print(decripteaza_mesajul(mesaj))
 
 if __name__ == "__main__":
     main()
