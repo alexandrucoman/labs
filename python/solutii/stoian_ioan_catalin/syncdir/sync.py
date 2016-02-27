0ae79c6570a7f4dd7e6eebb5f520718efa930a3f
StoianCatalin <shaggy.cata@gmail.com>
2016-02-28 01:13:48 +0200

    Am facut solutia pentru sincronizare orientata pe obiect

--- python/solutii/stoian_ioan_catalin/syncdir/sync.py
+++ python/solutii/stoian_ioan_catalin/syncdir/sync.py
@@ -8,74 +8,100 @@ import hashlib
 import time
 import shutil
 
-
-def searchfordifference(path_from, path_to):
-    """
-    This function search for difference between files
-    :param path_from:
-    :param path_to:
-    :return:
-    """
-    hasher_from = hashlib.md5()
-    hasher_to = hashlib.md5()
-    with open(path_from, "rb") as afile:
-        buffer = afile.read()
-        hasher_from.update(buffer)
-    with open(path_to, "rb") as afile:
-        buffer = afile.read()
-        hasher_to.update(buffer)
-    if hasher_from.digest() == hasher_to.digest():
-        return False
-    else:
-        return True
+class tree(object):
+    def __init__(self, path):
+        self.path = path
+        self.dir = {}
+        self.dir['last_mod'] = time.ctime(os.path.getmtime(self.path))
+        self.dir['path'] = path
+        self.dir['type'] = 0
+        self.dir['subdir'] = {}
+        self.build(self.path, self.dir)
 
 
-def syncFiles(from_file, to_file):
-    """
-    This function sync files from params
-    :param from_file:
-    :param to_file:
-    :return:
-    """
+    def build(self, path, dir, stare=False):
+        for file in os.listdir(path):
+            path_file = os.path.join(path, file)
+            if os.path.isdir(path_file):
+                dir['subdir'][path_file] = {
+                    "type": 0,
+                    "last_mod": time.ctime(os.path.getmtime(path_file)),
+                    "path": path_file,
+                    "subdir": {}
+                }
+                self.build(path_file, dir['subdir'][path_file])
+            if os.path.isfile(path_file):
+                dir['subdir'][path_file] = {
+                    "type": 1,
+                    "last_mod": time.ctime(os.path.getmtime(path_file)),
+                    "path": path_file,
+                    "edit": stare,
+                    "encrypt": self.encrypt(path_file)
+                }
 
 
-def syncallfiles(path_from, path_to, time_dir1, time_dir2):
-    """
-    This function find all files that have param key
-    :param path_from:
-    :param path_to:
-    :param time_dir1:
-    :param time_dir2:
-    :return:
-    """
-    for file in os.listdir(path_from):
-        file_from = os.path.join(path_from, file)
-        file_to = os.path.join(path_to, file)
-        if os.path.isdir(file_from):
-            if time_dir1 > time_dir2:
-                if not os.path.isdir(file_to):
-                    os.mkdir(file_to)
-            else:
-                if not os.path.isdir(file_to):
-                    shutil.rmtree(file_from)
-            syncallfiles(file_from, file_to, time_dir1, time_dir2)
-        if os.path.isfile(file_from):
-            if not os.path.isfile(file_to):
-                if time_dir1 > time_dir2:
-                    shutil.copyfile(file_from, file_to)
+    def encrypt(self, path):
+        hasher = hashlib.md5()
+        with open(path, "rb") as afile:
+            buffer = afile.read()
+            hasher.update(buffer)
+            return hasher.digest()
+
+
+    def auto_remove(self, dir):
+        if dir == None:
+            dir = self.dir
+        forRemove = []
+        if 'subdir' in dir:
+            for path in dir['subdir']:
+                if not os.path.exists(dir['subdir'][path]['path']):
+                    forRemove.append(path)
+                if os.path.isdir(dir['subdir'][path]['path']):
+                    self.auto_remove(dir['subdir'][path])
+
+        for item in forRemove:
+            del(dir['subdir'][item])
+
+
+    def update(self, path=None, dir=None):
+        if dir == None:
+            dir = self.dir
+        if path == None:
+            path = self.dir['path']
+        forRemove = []
+        for file in os.listdir(path):
+            path_file = os.path.join(path, file)
+            if os.path.isdir(path_file):
+                if path_file not in dir['subdir'].keys():
+                    dir['subdir'][path_file] = {
+                        "type": 0,
+                        "last_mod": time.ctime(os.path.getmtime(path_file)),
+                        "path": path_file,
+                        "subdir": {}
+                    }
+                    self.update(path_file, dir['subdir'][path_file])
                 else:
-                    os.remove(file_from)
-            else:
-                if searchfordifference(file_from, file_to):
-                    time_from = time.ctime(os.path.getmtime(file_from))
-                    time_to = time.ctime(os.path.getmtime(file_to))
-                    if time_from > time_to:
-                        os.remove(file_to)
-                        shutil.copyfile(file_from, file_to)
-                    else:
-                        os.remove(file_from)
-                        shutil.copyfile(file_to, file_from)
-                    #syncFiles(file_from, file_to)
+                    dir['subdir'][path_file]['last_mod'] = time.ctime(os.path.getmtime(path_file))
+                    self.update(path_file, dir['subdir'][path_file])
+            if os.path.isfile(path_file):
+                if path_file not in dir['subdir'].keys():
+                    dir['subdir'][path_file] = {
+                        "type": 1,
+                        "last_mod": time.ctime(os.path.getmtime(path_file)),
+                        "path": path_file,
+                        "edit": False,
+                        "encrypt": self.encrypt(path_file)
+                    }
+                else:
+                    hash = self.encrypt(path_file)
+                    if dir['subdir'][path_file]['encrypt'] != hash:
+                        dir['subdir'][path_file]['encrypt'] = hash
+                        dir['subdir'][path_file]['edit'] = True
+                        dir['subdir'][path_file]["last_mod"] = time.ctime(os.path.getmtime(path_file))
+        self.auto_remove(None)
+        return
+
+
 
 
 def main(first, second):
@@ -84,14 +110,12 @@ def main(first, second):
     :param first:
     :param second:
     """
+    tree1 = tree(first)
+    print(tree1.dir)
     while True:
-        time.sleep(0.2)
-        time_dir1 = time.ctime(os.path.getmtime(first))
-        time_dir2 = time.ctime(os.path.getmtime(second))
-        if time_dir1 > time_dir2:
-            syncallfiles(first, second, time_dir1, time_dir2)
-        else:
-            syncallfiles(second, first, time_dir2, time_dir1)
+        time.sleep(3)
+        tree1.update()
+        print(tree1.dir)
 
 if __name__ == "__main__":
     main("recursiv1", "recursiv2")
