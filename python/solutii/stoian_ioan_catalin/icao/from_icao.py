cf8a817dd1a96179224f2a750937e792453bf848
StoianCatalin <shaggy.cata@gmail.com>
2016-02-29 13:24:17 +0200

    Improve best practice from_icao

--- python/solutii/stoian_ioan_catalin/icao/from_icao.py
+++ python/solutii/stoian_ioan_catalin/icao/from_icao.py
@@ -18,6 +18,7 @@ Următoarea sarcină ți-a fost asignată:
 Mai jos găsiți un dicționar ce conține o versiune a alfabetului ICAO:
 """
 
+
 ICAO = {
     'a': 'alfa', 'b': 'bravo', 'c': 'charlie', 'd': 'delta', 'e': 'echo',
     'f': 'foxtrot', 'g': 'golf', 'h': 'hotel', 'i': 'india', 'j': 'juliett',
@@ -26,35 +27,43 @@ ICAO = {
     'u': 'uniform', 'v': 'victor', 'w': 'whiskey', 'x': 'x-ray', 'y': 'yankee',
     'z': 'zulu'
 }
-def getListOfLine(mesaj):
+
+
+def get_list_of_line(mesaj):
     "Imparte fisierul in linii"
     rezultat = mesaj.split("\n")
     return rezultat
-def getListOfWords(line):
+
+
+def get_list_of_words(line):
     "Imparte linia in cuvinte"
     rezultat = line.split(" ")
     return rezultat
-def getWordFromICAO(word):
+
+
+def get_word_from_icao(word):
     "Traduce cuvantul din ICAO in litera"
     for lit in ICAO:
         if ICAO[lit] == word:
             return lit
+
+
 def din_icao(mesaj):
     """Funcția va primi calea către fișierul ce conține mesajul brut și
     va genera un fișier numit icao_intrare ce va conține mesajul inițial.
     """
     fisier = open(mesaj, 'r')
-    lines = getListOfLine(fisier.read())
+    lines = get_list_of_line(fisier.read())
     fisier.close()
-    mesajDec = ""
+    mesajdec = ""
     for line in lines:
-        words = getListOfWords(line)
+        words = get_list_of_words(line)
         for word in words:
-            lit = getWordFromICAO(word)
-            if (lit):
-                mesajDec += lit
-        mesajDec += " "
-    print(mesajDec)
+            lit = get_word_from_icao(word)
+            if lit:
+                mesajdec += lit
+        mesajdec += " "
+    print(mesajdec)
     pass
 
 
