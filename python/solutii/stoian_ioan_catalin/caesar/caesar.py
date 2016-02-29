c01c459b058b9592f1781c44582f07a1bddea8de
StoianCatalin <shaggy.cata@gmail.com>
2016-02-29 13:24:01 +0200

    Improve best practice caesar

--- python/solutii/stoian_ioan_catalin/caesar/caesar.py
+++ python/solutii/stoian_ioan_catalin/caesar/caesar.py
@@ -14,23 +14,41 @@ la un n pași de ea în alfabet (unde este n este un număr întreg cunoscut
 """
 # pylint: disable=unused-argument
 
+
 from __future__ import print_function
 
+
 def getLit(lit, cifru):
+    """
+    this function return lit after decrypt
+    :param lit:
+    :param cifru:
+    :return:
+    """
     if lit.isalpha():
         litera = ord(lit) + cifru
-        if(litera > ord('z')):
+        if litera > ord('z'):
             character = (ord('a') - 1) + (litera - ord('z'))
         else:
             character = litera
         return chr(character)
     else:
-         return lit
+        return lit
 
-def match_cifru(a,b,c, cifru):
-    if (getLit(a, cifru)=="a" and getLit(b, cifru)=="v" and getLit(c, cifru)=="e"):
+
+def match_cifru(a, b, c, cifru):
+    """
+    function that check if cifru match with convention
+    :param a:
+    :param b:
+    :param c:
+    :param cifru:
+    :return:
+    """
+    if getLit(a, cifru) == "a" and getLit(b, cifru) == "v" and getLit(c, cifru) == "e":
         return True
     return False
+
 
 def decripteaza_mesajul(mesaj):
     """Funcția va primi un mesaj criptat folosind cifrul lui Caesar și
@@ -38,14 +56,13 @@ def decripteaza_mesajul(mesaj):
     """
     key = 0
     string = ""
-    for cifru in range(1,26):
+    for cifru in range(1, 26):
         if match_cifru(mesaj[0], mesaj[1], mesaj[2], cifru):
             key = cifru
     for lit in mesaj:
         c = getLit(lit, key)
         string += c
     print(string)
-    pass
 
 
 def main():
@@ -60,6 +77,7 @@ def main():
 
     for mesaj in mesaje.splitlines():
         decripteaza_mesajul(mesaj)
+
 
 if __name__ == "__main__":
     main()
