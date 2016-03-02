7fb10f3893877426f18162ea8cc51438795e60b1
StoianCatalin <shaggy.cata@gmail.com>
2016-03-02 15:53:06 +0200

    Adauga solutie pentru sincronizarea de directoare

--- python/solutii/stoian_ioan_catalin/syncdir/sync.py
+++ python/solutii/stoian_ioan_catalin/syncdir/sync.py
@@ -16,7 +16,7 @@ class Tree(object):
     def __init__(self, path):
         self.path = path
         self.dir = {}
-        self.dir['last_mod'] = time.ctime(os.path.getmtime(self.path))
+        self.dir['last_mod'] = self.getlastdate(self.path)
         self.dir['path'] = path
         self.dir['type'] = 0
         self.dir['subdir'] = {}
@@ -35,7 +35,7 @@ class Tree(object):
             if os.path.isdir(path_file):
                 dir['subdir'][path_file] = {
                     "type": 0,
-                    "last_mod": time.ctime(os.path.getmtime(path_file)),
+                    "last_mod": self.getlastdate(path_file),
                     "path": path_file,
                     "subdir": {}
                 }
@@ -43,7 +43,7 @@ class Tree(object):
             if os.path.isfile(path_file):
                 dir['subdir'][path_file] = {
                     "type": 1,
-                    "last_mod": time.ctime(os.path.getmtime(path_file)),
+                    "last_mod": self.getlastdate(path_file),
                     "path": path_file,
                     "edit": stare,
                     "encrypt": self.encrypt(path_file)
@@ -61,23 +61,6 @@ class Tree(object):
             hasher.update(buffer)
             return hasher.digest()
 
-    def auto_remove(self, dir):
-        """
-        this functon remove files that don't are in structure of tree.
-        :param dir:
-        :return:
-        """
-        if dir == None:
-            dir = self.dir
-        forremove = []
-        if 'subdir' in dir:
-            for path in dir['subdir']:
-                if not os.path.exists(dir['subdir'][path]['path']):
-                    forremove.append(path)
-                if os.path.isdir(dir['subdir'][path]['path']):
-                    self.auto_remove(dir['subdir'][path])
-        for item in forremove:
-            del(dir['subdir'][item])
 
     def update(self, path=None, dir=None):
         """
@@ -96,19 +79,19 @@ class Tree(object):
                 if path_file not in dir['subdir'].keys():
                     dir['subdir'][path_file] = {
                         "type": 0,
-                        "last_mod": time.ctime(os.path.getmtime(path_file)),
+                        "last_mod": self.getlastdate(path_file),
                         "path": path_file,
                         "subdir": {}
                     }
                     self.update(path_file, dir['subdir'][path_file])
                 else:
-                    dir['subdir'][path_file]['last_mod'] = time.ctime(os.path.getmtime(path_file))
+                    dir['subdir'][path_file]['last_mod'] = self.getlastdate(path_file)
                     self.update(path_file, dir['subdir'][path_file])
             if os.path.isfile(path_file):
                 if path_file not in dir['subdir'].keys():
                     dir['subdir'][path_file] = {
                         "type": 1,
-                        "last_mod": time.ctime(os.path.getmtime(path_file)),
+                        "last_mod": self.getlastdate(path_file),
                         "path": path_file,
                         "edit": False,
                         "encrypt": self.encrypt(path_file)
@@ -118,10 +101,88 @@ class Tree(object):
                     if dir['subdir'][path_file]['encrypt'] != hash:
                         dir['subdir'][path_file]['encrypt'] = hash
                         dir['subdir'][path_file]['edit'] = True
-                        path_time = os.path.getmtime(path_file)
-                        dir['subdir'][path_file]["last_mod"] = time.ctime(path_time)
-        self.auto_remove(None)
+                        dir['subdir'][path_file]["last_mod"] = self.getlastdate(path_file)
         return
+
+
+    def getlastdate(self, path):
+        """
+        returneaza ultima actualizare a unui fisier/folder
+        :param path:
+        :return:
+        """
+        modificare = os.path.getmtime(path)
+        return time.ctime(modificare)
+
+
+    def sync(self, structure1, structure2, path1, path2):
+        """
+        :param structure1:
+        :param structure2:
+        :return:
+        """
+        forremove = []
+        for path_2 in structure2['subdir']:
+            path_1 = path_2.replace(path2, path1)
+            if os.path.isfile(path_2):
+                #Este fisier dar nu se gaseste in al doilea folder.
+                if not os.path.isfile(path_1):
+                    #Daca fisierul a fost sters inainte de ultima modificare a celui existent
+                    if self.getlastdate(structure1['path']) > self.getlastdate(path_2):
+                        os.remove(path_2)
+                        print(path_2, " was removed.")
+                        forremove.append(path_2)
+                    else:
+                        shutil.copy(path_2, path_1)
+                        print(path_1, " sync with ", path_2)
+                #Este fisier, se gaseste si in al doilea folder, dar sunt diferite.
+                if os.path.isfile(path_1):
+                    if self.encrypt(path_1) != self.encrypt(path_2):
+                        #Daca fisierul din folder1 a fost modificat ultima data.
+                        if self.getlastdate(path_1) > self.getlastdate(path_2):
+                            shutil.copyfile(path_1, path_2)
+                            print(path_2, " sync with ", path_1)
+                        #Daca fisierul din folder2 a fost modificat ultima data.
+                        else:
+                            shutil.copyfile(path_2, path_1)
+                            print(path1, " sync with ", path_2)
+            if os.path.isdir(path_2):
+                #este director dar nu se gaseste in al doilea folder
+                if not os.path.isdir(path_1):
+                    if self.getlastdate(structure1['path']) > self.getlastdate(path_2):
+                        shutil.rmtree(path_2)
+                        print(path_2, " was removed.")
+                        forremove.append(path_2)
+                    else:
+                        shutil.copytree(path_2, path_1)
+                        print(path1, " sync with ", path_2)
+                if os.path.isdir(path_1) and path_1 in structure1['subdir'].keys():
+                    self.sync(structure1['subdir'][path_1], structure2['subdir'][path_2], path1, path2)
+        for item in forremove:
+            if item in structure1['subdir'].keys() and item in structure2['subdir'].keys():
+                del(structure1['subir'][item])
+                del(structure2['subdir'][item])
+            if item in structure1['subdir'].keys():
+                del(structure1['subdir'][item])
+            elif item in structure2['subdir'].keys():
+                del(structure2['subdir'][item])
+
+
+
+
+    def find_difference_with(self, with_dir):
+        """
+        :param dir:
+        :param with_dir:
+        :return:
+        """
+        structure1 = self.dir
+        structure2 = with_dir.dir
+        self.sync(structure1, structure2, self.path, with_dir.path)
+        self.sync(structure2, structure1, with_dir.path, self.path)
+        self.update()
+        with_dir.update()
+
 
 
 def main(first, second):
@@ -130,12 +191,11 @@ def main(first, second):
     :param first:
     :param second:
     """
-    tree1 = Tree(first)
-    print(tree1.dir)
+    director1 = Tree(first)
+    director2 = Tree(second)
     while True:
-        time.sleep(3)
-        tree1.update()
-        print(tree1.dir)
+        time.sleep(1)
+        director1.find_difference_with(director2)
 
 if __name__ == "__main__":
     main("recursiv1", "recursiv2")
