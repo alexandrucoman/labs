d875a24afad3055a3c700c58421631a19751de8f
Irina Bejan <irinam.bejan@gmail.com>
2016-03-27 23:11:15 +0300

    Unic new try

--- python/solutii/irina_bejan/unic.py
+++ python/solutii/irina_bejan/unic.py
@@ -15,7 +15,6 @@ Cerințe:
     II. Găsește cercetătorul ce stă peste program după o singură
     parcurgere a listei și fără a aloca memorie suplimentară.
 """
-# pylint: disable=unused-argument
 
 
 def gaseste_unic(istoric):
@@ -28,18 +27,29 @@ def gaseste_unic(istoric):
     """
     dic = {}
     for number in istoric:
-        if number in dic:
-            dic[number] += 1
-        else:
-            dic[number] = 1
-
+        dic[number] = dic.setdefault(number, 0) + 1
     for key, value in dic.iteritems():
         if value % 2 == 1:
             return key
 
 
+def gaseste_unic_versiune_trei(istoric):
+    """Găsește elementul unic in o(n) timp si o(1) memorie suplimentara
+    Funcția primește o listă cu elemente numerice și trebuie
+    să returneze elementul care nu este duplicat.
+    Exemple:
+        1 2 3 2 1 - 3
+        1 1 1 2 2 - 1
+    """
+    for element in istoric:
+        istoric[abs(element)] *= (-1)
+    for element in istoric:
+        if istoric[abs(element)] < 0:
+            return element
+
+
 def gaseste_unic_versiune_doi(istoric):
-    """Găsește elementul unic fara a aloca memorie suplimentara.
+    """Găsește elementul unic in o(nlgn) timp si o(1) memorie suplimentara
     Funcția primește o listă cu elemente numerice și trebuie
     să returneze elementul care nu este duplicat.
     Exemple:
@@ -66,5 +76,5 @@ def gaseste_unic_versiune_doi(istoric):
 
 
 if __name__ == "__main__":
-    assert gaseste_unic([1, 2, 3, 2, 1]) == 3
-    assert gaseste_unic([1, 1, 1, 2, 2]) == 1
+    assert gaseste_unic_versiune_trei([1, 2, 3, 2, 1]) == 3
+    assert gaseste_unic_versiune_trei([1, 1, 1, 2, 2]) == 1
