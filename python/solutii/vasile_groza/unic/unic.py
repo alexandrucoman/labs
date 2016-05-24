ef271eec507268a2ab7b85680fa56db264e80bdf
Vasile Groza <vasilegroza3@gmail.com>
2016-05-24 11:32:21 +0300

    Solved problem unic with suplimentar memory

---
+++ python/solutii/vasile_groza/unic/unic.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# *-* coding: UTF-8 *-*
+
+"""În laboratorul lui Tuxy toți cercetătorii au asignat un id
+de utilizator.
+
+Pentru fiecare cercetător se salvează într-o listă de fiecare
+dată când a deschis ușa (fie pentru a intra, fie pentru a ieși).
+
+Tuxy suspectează că cineva rămâne tot timpul după program și
+ar dori să scrie un script care să îi verifice teoria, dar
+nu a reușit pentru că algoritmul său era prea costisitor pentru
+sistem.
+
+Cerințe:
+    I. Găsește cercetătorul ce stă peste program după o singură
+    parcurgere a listei
+    II. Găsește cercetătorul ce stă peste program după o singură
+    parcurgere a listei și fără a aloca memorie suplimentară.
+"""
+
+
+# pylint: disable=unused-argument
+from array import array
+
+
+def gaseste_unic(istoric):
+    """Găsește elementul unic.
+
+    Funcția primește o listă cu elemente numerice și trebuie
+    să returneze elementul care nu este duplicat.
+
+    Exemple:
+        1 2 3 2 1 - 3
+        1 1 1 2 2 - 1
+    """
+    accesari =array('i',(0 for i in range(0,10)))
+    idHardWorker = -1
+    for id in istoric:
+        accesari[id] = accesari[id]+ 1
+
+    for index in range(0, len(accesari)):
+        if accesari[index] % 2 == 1:
+            idHardWork = index
+    print (idHardWorker)
+    return idHardWorker
+
+
+if __name__ == "__main__":
+    assert gaseste_unic([1, 2, 3, 2, 1]) == 3
+    assert gaseste_unic([1, 1, 1, 2, 2]) == 1
