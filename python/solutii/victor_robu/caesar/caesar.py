7c7111113aa3737d014f3f6d49e2540ac086ead7
Victor Robu <robu_ved@yahoo.com>
2016-05-25 08:41:38 +0300

    Adaugat solutie caesar

--- python/solutii/victor_robu/caesar/caesar.py
+++ python/solutii/victor_robu/caesar/caesar.py
@@ -16,24 +16,53 @@ la un n pași de ea în alfabet (unde este n este un număr întreg cunoscut
 
 from __future__ import print_function
 
-
+primCar=-1
 def decripteaza_mesajul(mesaj):
     """Funcția va primi un mesaj criptat folosind cifrul lui Caesar și
     va încearca să îl decripteze.
     """
-    pass
-
+    noOfChars = ord('z')-ord('a')+1
+    global primCar
+    if primCar < 0:
+	for deplasare in range(noOfChars):
+	    buf=""
+	    for litera in mesaj:
+             	if litera.isalpha():
+		    buf+=chr((ord(litera)+deplasare-ord('a'))%noOfChars + ord('a')) 
+	        else:
+		    buf+=litera
+	    print(deplasare,": ",buf)
+	flag = 1
+	while flag:
+	     print ("Alegeti unul din siruri introducand un numar intre 0 si",noOfChars-1)
+	     try:
+		opt=int(raw_input('Input:'))
+	     except ValueError:
+	     	print("Not a number")
+	     else:
+		if opt>=0 and opt<noOfChars:
+		    flag=0
+	primCar=(ord(mesaj[0])+opt-ord('a'))%noOfChars
+    else:
+	cheieInv=(primCar-ord(mesaj[0])+ord('a'))%noOfChars
+	buf=""
+	for litera in mesaj:
+	    if litera.isalpha():
+		buf+=chr((ord(litera)+cheieInv-ord('a'))%noOfChars + ord('a'))
+	    else:
+		buf+=litera
+	print("Cheie :",noOfChars-cheieInv,". Mesaj :",buf)
 
 def main():
     """ Main function docstring """
     try:
-        fisier = open("mesaje.secret", "r")
+        fisier = open("../../../date_intrare/mesaje.secret", "r")
         mesaje = fisier.read()
         fisier.close()
     except IOError:
         print("Nu am putut obține mesajele.")
         return
-
+    
     for mesaj in mesaje.splitlines():
         decripteaza_mesajul(mesaj)
 
