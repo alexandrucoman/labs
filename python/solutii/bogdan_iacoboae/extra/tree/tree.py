c3bd3df5faa5dc43cdec8467156ff65bd0c160ba
Iacoboae Bogdan <bogdan.iacoboae@info.uaic.ro>
2016-06-04 13:14:32 +0300

    PEP8 on Tree

--- python/solutii/bogdan_iacoboae/extra/tree/tree.py
+++ python/solutii/bogdan_iacoboae/extra/tree/tree.py
@@ -6,11 +6,11 @@ def list_files(locatie):
     for root, dirs, files in os.walk(locatie):
         nivel = root.replace(locatie, '').count(os.sep)
         spatiu = '-' * 4 * (nivel)
-        print '[DIR ]|%s%s' % (spatiu,os.path.basename(root))
+        print '[DIR ]|%s%s' % (spatiu, os.path.basename(root))
         spatiu2 = '-' * 4 * (nivel + 1)
         for fisier in files:
-            print '[FILE]|%s%s' % (spatiu2,fisier)
+            print '[FILE]|%s%s' % (spatiu2, fisier)
 
 if __name__ == "__main__":
-    foldername= tkFileDialog.askdirectory()
+    foldername = tkFileDialog.askdirectory()
     list_files(foldername)
