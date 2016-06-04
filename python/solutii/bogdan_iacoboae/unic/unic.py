05fb54021de8dfa3082e4ab4e6e0d5cef1ad7143
Iacoboae Bogdan <bogdan.iacoboae@info.uaic.ro>
2016-06-04 13:19:26 +0300

    PEP8 on unic

--- python/solutii/bogdan_iacoboae/unic/unic.py
+++ python/solutii/bogdan_iacoboae/unic/unic.py
@@ -1,4 +1,3 @@
-
 def gaseste_unic(istoric):
     unic = istoric.pop(0)
     for index in istoric:
@@ -10,8 +9,8 @@ if __name__ == "__main__":
     # lista = [1, 2, 3, 2, 1]
     lista = [1, 1, 1, 2, 2]
     print "Lista id-uri cercetatori: ",
-    for index in lista :
+    for index in lista:
         print index,
     print""
-    print "Cercetatorul cu id-ul %s este cercetatorul care sta peste program" % (gaseste_unic(lista))
-
+    print "Cercetatorul cu id-ul %s este " \
+          "cercetatorul care sta peste program" % (gaseste_unic(lista))
