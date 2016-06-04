e0e92e381eda50c2147e9a2d7f546d4b5a7ae941
Iacoboae Bogdan <bogdan.iacoboae@info.uaic.ro>
2016-06-04 13:13:30 +0300

    PEP8 on Putere

--- python/solutii/bogdan_iacoboae/extra/putere/putere.py
+++ python/solutii/bogdan_iacoboae/extra/putere/putere.py
@@ -1,13 +1,14 @@
 __author__ = 'kali'
 
+
 def power(number):
     binar = bin(number)[2:]
     # binar = "{0:b}".format(number)
-    if binar.count("1")==1 :
+    if binar.count("1") == 1:
         print "Este putere a lui doi"
-    else :
+    else:
         print "Nu este putere a lui doi"
 
 if __name__ == "__main__":
-    input=int(raw_input("Gimme the number: "))
+    input = int(raw_input("Gimme the number: "))
     power(input)
